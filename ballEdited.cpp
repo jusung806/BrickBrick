1bd0640446022a899b30adea66b66462a5f946b5
Thomas-Stanton <55109510+Thomas-Stanton@users.noreply.github.com>
2019-11-27 15:41:40 -0800

    Add files via upload
    Same comment that is on the .h file

---
+++ ballEdited.cpp
@@ -0,0 +1,54 @@
+#include "Ball.h"
+
+Ball::Ball(SDL_Renderer* renderer) : Entity(renderer) {
+	SDL_Surface* surface = IMG_Load("ball.png");
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	x = 0;
+	y = 0;
+	width = 24;
+	height = 24;
+
+	SetDirection(1, 1);
+}
+
+Ball::~Ball() {
+	// Clean resources
+	SDL_DestroyTexture(texture);
+}
+
+void Ball::Update(float delta) {
+	x += dirx * delta;
+	y += diry * delta;
+}
+
+
+
+
+
+void Ball::Render(float delta) {
+	SDL_Rect rect;
+	rect.x = (int)(x + 1f);
+	rect.y = (int)(y + 1f);
+	rect.w = width;
+	rect.h = height;
+	SDL_RenderCopy(renderer, texture, 0, &rect);
+}
+
+
+
+void Ball::SetDirection(float dirx, float diry, const Paddle &paddle)
+{
+	if (paddle.fireball == true)
+	{
+		this->dirx = paddle.dirx;
+	}
+	// Normalize the direction vector and multiply with BALL_SPEED
+	else
+	{
+		float length = sqrt(dirx * dirx + diry * diry);
+		this->dirx = BALL_SPEED * (dirx / length);
+		this->diry = BALL_SPEED * (diry / length);
+	}
+}
