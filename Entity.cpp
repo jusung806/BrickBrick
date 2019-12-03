a735c1c264d13ffa4b792563cecc576eda178fc4
jusung806 <jusung806@gmail.com>
2019-12-03 09:34:00 -0800

    new update
    working

--- Entity.cpp
+++ Entity.cpp
@@ -1,10 +1,12 @@
-Entity::Entity(SDL_Renderer* renderer) {
-    this->renderer = renderer;
+#include "Entity.h"
 
-    x = 0;
-    y = 0;
-    width = 1;
-    height = 1;
+Entity::Entity(SDL_Renderer* renderer) {
+	this->renderer = renderer;
+
+	x = 0;
+	y = 0;
+	width = 1;
+	height = 1;
 }
 
 Entity::~Entity() {
@@ -17,9 +19,9 @@ void Entity::Render(float delta) {
 }
 
 bool Entity::Collides(Entity* other) {
-    if (x + width > other->x && x < other->x + other->width &&
-        y + height > other->y && y < other->y + other->height) {
-        return true;
-    }
-    return false;
+	if (x + width > other->x && x < other->x + other->width &&
+		y + height > other->y && y < other->y + other->height) {
+		return true;
+	}
+	return false;
 }
