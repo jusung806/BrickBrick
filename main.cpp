4ae35a213b5c0b50d7c4921aed5b36fc0eafc0ca
jusung806 <jusung806@gmail.com>
2019-12-03 09:45:58 -0800

    working

--- main.cpp
+++ main.cpp
@@ -3,170 +3,26 @@
 #define SDL_MAIN_HANDLED
 
 #include <SDL.h>
-//#include <SDL_mixer.h>
+#include "SDL_image.h"
+#include "Paddle.h"
+#include "Ball.h"
+#include "Audio.h"
+#include "Tile.h"
+#include "Entity.h"
+#include "game.h"
+#include <SDL_mixer.h>
 #include <string>
 
-
 using namespace std;
-
-class Audio {
-public:
-	Audio();
-	void load(const char* filename);
-	void play();
-private:
-	SDL_AudioSpec wavSpec;
-	Uint32 wavLength;
-	Uint8* wavBuffer;
-	SDL_AudioDeviceID deviceID;
-};
-
-Audio::Audio() {
-	SDL_CloseAudioDevice(deviceID);
-	SDL_FreeWAV(wavBuffer);
-}
-
-void Audio::load(const char* filename)
-{
-	if (SDL_LoadWAV(filename, &wavSpec, &wavBuffer, &wavLength) == NULL)
-	{
-		return;
-	}
-	SDL_LoadWAV(filename, &wavSpec, &wavBuffer, &wavLength);
-	deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
-
-
-}
-
-void Audio::play()
-{
-	SDL_QueueAudio(deviceID, wavBuffer, wavLength);
-	SDL_PauseAudioDevice(deviceID, 0);
-}
 
 // test program 
 
 class GameState {
 public:
-	float player_x; //need to add more here
+	float player; //need to add more here
 	// wouldn't this be connected to paddle, like this?
-	// Paddle player1;
 };
 
-class Game {
-public:
-	Game();
-	~Game();
-
-	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
-
-	void handleEvents();
-	void update();
-	void render();
-	void clean();
-	void input();
-
-	bool running() { return isRunning; }
-private:
-	int counter; //this is just to see it updating on console;
-	bool isRunning;
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-};
-
-Game::Game()
-{
-}
-
-Game::~Game()
-{
-}
-
-void Game::init(const char * title, int xpos, int ypos, int width, int height, bool fullscreen)
-{
-	int flags = 0;
-
-	if (fullscreen)
-	{
-		flags = SDL_WINDOW_FULLSCREEN;
-	}
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-	{
-		cout << "Subsystems Initialised!..." << endl;
-
-		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (window)
-		{
-			cout << "Window created!" << endl;
-
-		}
-		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer)
-		{
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			cout << "Renderer created!" << endl;
-		}
-
-		isRunning = true;
-	}
-	else {
-		isRunning = false;
-	}
-}
-
-void Game::handleEvents()
-{
-	SDL_Event event;
-	SDL_PollEvent(&event);
-	switch (event.type) 
-	{
-	case SDL_QUIT:
-		isRunning = false;
-		break;
-
-  /*
-    case SDL_SCANCODE_RIGHT:
-		rect.x += 1; this should be changed to gamestate.player1.dirx+=1;
-		break;
-	case SDL_SCANCODE_LEFT:
-		rect.x -= 1; this should be changed to gamestate.player1.dirx-=1;
-		break;
-
-	case SDL_SCANCODE_KP_SPACE
-			this should be gamestate.player1.fireball = true;
-			break;`
-		*/
-	
-
-	default:
-		break;
-	}
-}
-
-void Game::update() {
-	counter++;  //will delete later this is where the player will be updated
-	cout << counter << endl;
-}
-
-void Game::render() {
-	SDL_RenderClear(renderer);
-	// this is where we would add stuff to render
-	SDL_RenderPresent(renderer);
-}
-
-void Game::clean()
-{
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
-	cout << "Game Cleaned" << endl;
-}
-
-void Game::input() 
-{
-
-}
 
 Game *game = nullptr;
 
@@ -199,23 +55,12 @@ int main()
 
 		//game->input();
 		game->handleEvents(); // i think this is input
-		
-	
+
+
 		game->update();
-		
-		
+
+
 		game->render();
-
-		//double current = getCurrentTime(); these two functions will keep the game from running to slowly or to quickly between frames of the ball and player moving
-		//double elapsed = current - lastTime;
-		// processInput(); this function will process user input calls on sub system user input functions -user input(player movement, ball being shot)
-		// update(elasped); this funtion should update the input to the system 
-		// render(); this should update the system to the screen the ball moving across the screen the player see's this
-		//lastTime = current;
-
-		// update(&gamestate); 
-
-		// draw(&gamestate);
 
 	}
 
