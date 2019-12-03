3a3098939175bbd31b559c1cd7338c6ba58b8a27
jusung806 <jusung806@gmail.com>
2019-12-03 09:37:44 -0800

    working

--- Ball.h
+++ Ball.h
@@ -1,32 +1,35 @@
-//
-// Created by jusung on 11/25/19.
-//
-
 #ifndef BRICKBRICKBALLV2_BALL_H
 #define BRICKBRICKBALLV2_BALL_H
 
+#define SDL_MAIN_HANDLED
 
+#include <SDL.h>
 #include "Entity.h"
+#include "Paddle.h"
 
 #include <math.h>
 
 // Define a ball speed in pixels per second
 const float BALL_SPEED = 550;
 
-class Ball: public Entity {
+class Ball : public Entity
+{
 public:
-    Ball(SDL_Renderer* renderer);
-    ~Ball();
+	Ball(SDL_Renderer* renderer);
+	~Ball();
 
-    void Update(float delta);
-    void Render(float delta);
+	void Update(float delta);
+	void Render(float delta);
 
-    void SetDirection(float dirx, float diry);
+	void SetDirection(float dirx, float diry, const Paddle &paddle);
 
-    float dirx, diry;
+	float dirx, diry;
+
+
+
 
 private:
-    SDL_Texture* texture;
+	SDL_Texture* texture;
 
 };
 
