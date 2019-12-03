89a864ffdc8e42652636a02bd630463c45aadf07
jusung806 <jusung806@gmail.com>
2019-12-03 09:35:41 -0800

    working

--- Ball.cpp
+++ Ball.cpp
@@ -1,40 +1,54 @@
 #include "Ball.h"
 
-Ball::Ball(SDL_Renderer* renderer): Entity(renderer) {
-    SDL_Surface* surface = IMG_Load("ball.png");
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+Ball::Ball(SDL_Renderer* renderer) : Entity(renderer) {
+	SDL_Surface* surface = IMG_Load("ball.png");
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
 
-    x = 0;
-    y = 0;
-    width = 24;
-    height = 24;
+	x = 0;
+	y = 0;
+	width = 24;
+	height = 24;
 
-    SetDirection(1, 1);
+	SetDirection(1, 1, NULL);
 }
 
 Ball::~Ball() {
-    // Clean resources
-    SDL_DestroyTexture(texture);
+	// Clean resources
+	SDL_DestroyTexture(texture);
 }
 
 void Ball::Update(float delta) {
-    x += dirx * delta;
-    y += diry * delta;
+	x += dirx * delta;
+	y += diry * delta;
 }
+
+
+
+
 
 void Ball::Render(float delta) {
-    SDL_Rect rect;
-    rect.x = (int)(x + 0.5f);
-    rect.y = (int)(y + 0.5f);
-    rect.w = width;
-    rect.h = height;
-    SDL_RenderCopy(renderer, texture, 0, &rect);
+	SDL_Rect rect;
+	rect.x = (int)(x + 0.5f);
+	rect.y = (int)(y + 0.5f);
+	rect.w = width;
+	rect.h = height;
+	SDL_RenderCopy(renderer, texture, 0, &rect);
 }
 
-void Ball::SetDirection(float dirx, float diry) {
-    // Normalize the direction vector and multiply with BALL_SPEED
-    float length = sqrt(dirx * dirx + diry * diry);
-    this->dirx = BALL_SPEED * (dirx / length);
-    this->diry = BALL_SPEED * (diry / length);
+
+
+void Ball::SetDirection(float dirx, float diry, const Paddle &paddle)
+{
+	if (paddle.fireball == true)
+	{
+		this->dirx = paddle.dirx;
+	}
+	// Normalize the direction vector and multiply with BALL_SPEED
+	else
+	{
+		float length = sqrt(dirx * dirx + diry * diry);
+		this->dirx = BALL_SPEED * (dirx / length);
+		this->diry = BALL_SPEED * (diry / length);
+	}
 }
