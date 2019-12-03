df092e23b80a91d12587a32d9e5deb77f283dc3c
jusung806 <jusung806@gmail.com>
2019-12-03 09:42:37 -0800

    working

--- Paddle.h
+++ Paddle.h
@@ -1,20 +1,28 @@
-//
-// Created by jusung on 11/25/19.
-//
-
 #ifndef BRICKBRICKBALLV2_PADDLE_H
 #define BRICKBRICKBALLV2_PADDLE_H
 
-class Paddle: public Entity {
-public:
-    Paddle(SDL_Renderer* renderer);
-    ~Paddle();
+#define SDL_MAIN_HANDLED
 
-    void Update(float delta);
-    void Render(float delta);
+#include <SDL.h>
+#include "Entity.h"
+
+
+class Paddle : public Entity
+{
+public:
+	Paddle(SDL_Renderer* renderer);
+	~Paddle();
+
+	void Update(float delta);
+	void Render(float delta);
+	bool fireball;
+
+	float dirx;
+
 
 private:
-    SDL_Texture* texture;
+	SDL_Texture* texture;
+
 };
 
 #endif //BRICKBRICKBALLV2_PADDLE_H
